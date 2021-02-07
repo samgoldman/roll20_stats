a66666715d89e9d0513f6ef371bc90a8f70b9f9d
Sam Goldman <sgoldman216@gmail.com>
2021-02-07 10:52:09 -0700

    Fix 2nd parsing method

--- parser.py
+++ parser.py
@@ -27,7 +27,8 @@ for message in messages:
     if not by is None:
         most_recent_by = by.text[:-1]
         if names is not None:
-            most_recent_by = names[most_recent_by]
+            if most_recent_by in names:
+                most_recent_by = names[most_recent_by]
 
     if not most_recent_by in rolls:
         rolls[most_recent_by] = []
@@ -40,7 +41,7 @@ for message in messages:
     for formula in formulas:
         if formula.text.startswith('rolling d20') or formula.text.startswith('rolling 1d20'):
             is_plain_d20 = True
-
+    
     if is_plain_d20:
         for roll_div in roll_divs:
             try:
@@ -48,16 +49,19 @@ for message in messages:
             except:
                 pass
 
-
     roll_divs = message.find_all('span', {'class': 'inlinerollresult'})
     for roll_div in roll_divs:
         title = roll_div['title']
         if title.startswith('Rolling 1d20') or title.startswith('Rolling d20'):
             result = (re.search('>(\d+)<', title)).group(1)
-        try:
+            try:
                 rolls[most_recent_by].append(int(result))
-        except:
-            pass
+            except:
+                pass
+
+for key in list(rolls):
+    if len(rolls[key]) == 0:
+        del rolls[key]
 
 for key, l in sorted(rolls.items(), key=lambda item: mean(item[1]), reverse=True):
     try:
